df3c74e7607459c82f96d8cc678c4edb840068f0
Toby Potter <tobympotter@gmail.com>
2023-01-13 14:41:40 +1100

    latest

--- course_material/L3_Matrix_Multiplication/mat_mult.cpp
+++ course_material/L3_Matrix_Multiplication/mat_mult.cpp
@@ -57,13 +57,13 @@ int main(int argc, char** argv) {
     
     //// Step 1. Parse program arguments ////
 
-    // Parse arguments and set the target device
+    // Parse arguments
     int dev_index = h_parse_args(argc, argv);
     
     // Number of devices discovered
     int num_devices;
     
-    //// Step 2. Discover resources ////
+    //// Step 2. Discover resources and choose a compute device ////
     
     // Helper function to acquire devices
     // This sets the default device
@@ -81,8 +81,10 @@ int main(int argc, char** argv) {
 
     size_t N1_A = NCOLS_A, N0_C = NROWS_C, N1_C = NCOLS_C;
 
-    //// Step 3. Allocate memory for arrays A and B on the host ////
-    //// and fill with random numbers ////
+    //// Step 3. 1. Construct matrices A_h and B_h on the host 
+    //// and fill them with random numbers ////
+    
+    // Number of bytes in each array
     size_t nbytes_A = N0_C*N1_A*sizeof(float);
     size_t nbytes_B = N1_A*N1_C*sizeof(float);
     size_t nbytes_C = N0_C*N1_C*sizeof(float);
@@ -98,19 +100,21 @@ int main(int argc, char** argv) {
     m_random(A_h, N0_C, N1_A);
     m_random(B_h, N1_A, N1_C);
     
-    //// Step 4. Allocate on-device memory for matrices A, B, and C ////
+    //// Step 4. Allocate memory for arrays //// 
+    //// A_d, B_d, and C_d on the compute device ////
 
     float *A_d, *B_d, *C_d;
     H_ERRCHK(hipMalloc((void**)&A_d, nbytes_A));
     H_ERRCHK(hipMalloc((void**)&B_d, nbytes_B));
     H_ERRCHK(hipMalloc((void**)&C_d, nbytes_C));
 
-    //// Step 5. Upload matrices A and B from the host
-    // to the HIP device allocations ////
+    //// Step 5. 1. Upload matrices A_h and B_h from the host //// 
+    //// to A_d and B_d on the device ////
     H_ERRCHK(hipMemcpy(A_d, A_h, nbytes_A, hipMemcpyHostToDevice));
     H_ERRCHK(hipMemcpy(B_d, B_h, nbytes_B, hipMemcpyHostToDevice));
  
-    //// Step 6. Run the kernel to compute C from A and B ////
+    //// Step 6. Run the kernel to compute C_d ///
+    //// from A_d and B_d on the device ////
         
     // Desired block size
     dim3 block_size = { 8, 8, 1 };
@@ -133,17 +137,17 @@ int main(int argc, char** argv) {
             N1_C
     );
     
-    // Launch the kernel using CUDA triple Chevron syntax
+    // Alternatively, launch the kernel using CUDA triple Chevron syntax
     //mat_mult<<<grid_nblocks, block_size, 0, 0>>>(A_d, B_d, C_d, N1_A, N0_C, N1_C);
     
     // Wait for any commands to complete on the compute device
     H_ERRCHK(hipDeviceSynchronize());
 
-    //// Step 7. Copy the Buffer for matrix C back to the host ////
+    //// Step 7. Copy the buffer for matrix C_d //// 
+    //// on the device back to C_h on the host ////
     H_ERRCHK(hipMemcpy((void*)C_h, (const void*)C_d, nbytes_C, hipMemcpyDeviceToHost));
     
-    //// Step 8. Test against a known answer,
-    //// and write the contents of matrix C to disk
+    //// Step 8. Test the computed matrix **C_h** against a known answer
     
     // Compute the serial solution using the matrix helper library
     float* C_answer_h = (float*)calloc(nbytes_C, 1);
@@ -152,19 +156,21 @@ int main(int argc, char** argv) {
     // Print the maximum error between matrices
     float max_err = m_max_error(C_h, C_answer_h, N0_C, N1_C);
     
+    //// Step 9. Write the contents of matrices A_h, B_h, and C_h to disk ////
+
     // Write out the host arrays to file
     h_write_binary(A_h, "array_A.dat", nbytes_A);
     h_write_binary(B_h, "array_B.dat", nbytes_B);
     h_write_binary(C_h, "array_C.dat", nbytes_C);
     
-    //// Step 9. Clean up arrays and release resources
+    //// Step 10. Clean up memory alllocations and release resources
     
     // Free the HIP buffers
     H_ERRCHK(hipFree(A_d));
     H_ERRCHK(hipFree(B_d));
     H_ERRCHK(hipFree(C_d));
 
-    // Clean up memory that was allocated on the read   
+    // Clean up pinned memory on the host   
     H_ERRCHK(hipHostFree(A_h));
     H_ERRCHK(hipHostFree(B_h));
     H_ERRCHK(hipHostFree(C_h));
