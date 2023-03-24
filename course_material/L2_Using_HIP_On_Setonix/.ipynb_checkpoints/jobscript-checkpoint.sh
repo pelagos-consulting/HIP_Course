a2b16590bda305a1d0ec9aa53e8a3e7fe3e146bf
Toby Potter <tobympotter@gmail.com>
2023-03-24 15:51:21 +1100

    Latest

--- course_material/L2_Using_HIP_On_Setonix/.ipynb_checkpoints/jobscript-checkpoint.sh
+++ course_material/L2_Using_HIP_On_Setonix/.ipynb_checkpoints/jobscript-checkpoint.sh
@@ -6,9 +6,6 @@
 #SBATCH --ntasks-per-node=6        # Set this for 1 mpi task per compute device
 #SBATCH --cpus-per-task=8          # How many OpenMP threads per MPI task 
 #SBATCH --gpus-per-task=1          # How many HIP compute devices to allocate to a  task
-#SBATCH --cpu-bind=ldoms           # Bind by NUMA domain
-#SBATCH -m plane=4                 # Set this to the number of NUMA domains on a processor
-#SBATCH --gpu-bind=closest         # Choose the closest GPU to each task
 #SBATCH --mem=4000M                #Indicate the amount of memory per node when asking for share resources
 #SBATCH --time=01:00:00
 
@@ -20,11 +17,11 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK   #To define the number of OpenMP th
 export OMP_PLACES=cores     #To bind threads to cores
 export OMP_PROC_BIND=close  #To bind (fix) threads (allocating them as close as possible). This option works together with the "places" indicated above, then: allocates threads in closest cores.
  
-# ---
 # Temporal workaround for avoiding Slingshot issues on shared nodes:
 export FI_CXI_DEFAULT_VNI=$(od -vAn -N4 -tu < /dev/urandom)
 
-# Run the desired code:
-srun rocm-smi --showbus
-echo "Number of nodes is ${SLURM_JOB_NUM_NODES}, number of tasks is $SLURM_NTASKS, number of cores per task is $OMP_NUM_THREADS"
-srun -N $SLURM_JOB_NUM_NODES -n $SLURM_NTASKS -c $OMP_NUM_THREADS  ./hello_devices_mpi_onefile_hipcc.exe
+# Bind options to srun plane=4 is the number of NUMA domains per processor (4)
+export BIND_OPTIONS="--cpu-bind=ldoms -m plane=4 --gpu-bind=closest"
+
+# Run a job with task placement and $BIND_OPTIONS
+srun -N $SLURM_JOB_NUM_NODES -n $SLURM_NTASKS -c $OMP_NUM_THREADS $BIND_OPTIONS  ./hello_jobstep.exe
--- course_material/L2_Using_HIP_On_Setonix/jobscript.sh
+++ course_material/L2_Using_HIP_On_Setonix/jobscript.sh
@@ -17,16 +17,11 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK   #To define the number of OpenMP th
 export OMP_PLACES=cores     #To bind threads to cores
 export OMP_PROC_BIND=close  #To bind (fix) threads (allocating them as close as possible). This option works together with the "places" indicated above, then: allocates threads in closest cores.
  
-# ---
 # Temporal workaround for avoiding Slingshot issues on shared nodes:
 export FI_CXI_DEFAULT_VNI=$(od -vAn -N4 -tu < /dev/urandom)
 
-# Options to SRUN
+# Bind options to srun plane=4 is the number of NUMA domains per processor (4)
 export BIND_OPTIONS="--cpu-bind=ldoms -m plane=4 --gpu-bind=closest"
 
-# Run the desired code:
-#srun $BIND_OPTIONS rocm-smi --showbus
-
-# Run a job with task placement bound to NUMA domains with a plane size of 4
-#echo "Number of nodes is ${SLURM_JOB_NUM_NODES}, number of tasks is $SLURM_NTASKS, number of cores per task is $OMP_NUM_THREADS"
+# Run a job with task placement and $BIND_OPTIONS
 srun -N $SLURM_JOB_NUM_NODES -n $SLURM_NTASKS -c $OMP_NUM_THREADS $BIND_OPTIONS  ./hello_jobstep.exe
