f72b218c7d291bb03b97ca09320c77dfd3603239
Toby Potter <tobympotter@gmail.com>
2023-03-25 13:50:37 +0800

    latest

--- course_material/L2_Using_HIP_On_Setonix/jobscript.sh
+++ course_material/L2_Using_HIP_On_Setonix/jobscript.sh
@@ -2,9 +2,10 @@
 
 #SBATCH --account=pawsey0007       # your account
 #SBATCH --partition=gpu            # Using the gpu partition
-#SBATCH --ntasks=6                 # Total number of tasks
-#SBATCH --ntasks-per-node=6        # Set this for 1 mpi task per compute device
-#SBATCH --cpus-per-task=8          # How many OpenMP threads per MPI task 
+#SBATCH --ntasks=8                 # Total number of tasks
+#SBATCH --ntasks-per-node=8        # Set this for 1 mpi task per compute device
+#SBATCH --cpus-per-task=16         # How many OpenMP threads per MPI task 
+#SBATCH --threads-per-core=2	   # How many threads per core
 #SBATCH --gpus-per-task=1          # How many HIP compute devices to allocate to a  task
 #SBATCH --mem=4000M                #Indicate the amount of memory per node when asking for share resources
 #SBATCH --time=01:00:00
@@ -14,7 +15,7 @@ module load craype-accel-amd-gfx90a
 module load rocm
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK   #To define the number of OpenMP threads available per MPI task, in this case it will be 8
-export OMP_PLACES=cores     #To bind threads to cores
+export OMP_PLACES=threads     #To bind threads to cores
 export OMP_PROC_BIND=close  #To bind (fix) threads (allocating them as close as possible). This option works together with the "places" indicated above, then: allocates threads in closest cores.
  
 # Temporal workaround for avoiding Slingshot issues on shared nodes:
