a357906665a734865f39e893bff7426c47b0796f
Toby Potter <tobympotter@gmail.com>
2023-03-27 14:24:05 +1100

    latest

--- course_material/L2_Using_HIP_On_Setonix/.ipynb_checkpoints/jobscript-checkpoint.sh
+++ course_material/L2_Using_HIP_On_Setonix/.ipynb_checkpoints/jobscript-checkpoint.sh
@@ -2,10 +2,13 @@
 
 #SBATCH --account=pawsey0007       # your account
 #SBATCH --partition=gpu            # Using the gpu partition
-#SBATCH --ntasks=6                 # Total number of tasks
-#SBATCH --ntasks-per-node=6        # Set this for 1 mpi task per compute device
-#SBATCH --cpus-per-task=8          # How many OpenMP threads per MPI task 
+#SBATCH --ntasks=8                 # Total number of tasks
+#SBATCH --ntasks-per-node=8        # Set this for 1 mpi task per compute device
+#SBATCH --cpus-per-task=8          # How many OpenMP threads per MPI task
+#SBATCH --threads-per-core=1       # How many omp threads per core
+#SBATCH --gpus-per-node=8          # How many GPU's per node
 #SBATCH --gpus-per-task=1          # How many HIP compute devices to allocate to a  task
+#SBATCH --gpu-bind=closest         # Bind each MPI taks to the nearest GPU
 #SBATCH --mem=4000M                #Indicate the amount of memory per node when asking for share resources
 #SBATCH --time=01:00:00
 
@@ -14,14 +17,12 @@ module load craype-accel-amd-gfx90a
 module load rocm
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK   #To define the number of OpenMP threads available per MPI task, in this case it will be 8
-export OMP_PLACES=cores     #To bind threads to cores
+export OMP_PLACES=cores     #To bind to cores 
 export OMP_PROC_BIND=close  #To bind (fix) threads (allocating them as close as possible). This option works together with the "places" indicated above, then: allocates threads in closest cores.
  
 # Temporal workaround for avoiding Slingshot issues on shared nodes:
 export FI_CXI_DEFAULT_VNI=$(od -vAn -N4 -tu < /dev/urandom)
 
-# Bind options to srun plane=4 is the number of NUMA domains per processor (4)
-export BIND_OPTIONS="--cpu-bind=ldoms -m plane=4 --gpu-bind=closest"
-
 # Run a job with task placement and $BIND_OPTIONS
-srun -N $SLURM_JOB_NUM_NODES -n $SLURM_NTASKS -c $OMP_NUM_THREADS $BIND_OPTIONS  ./hello_jobstep.exe
+#srun -N $SLURM_JOB_NUM_NODES -n $SLURM_NTASKS -c $OMP_NUM_THREADS $BIND_OPTIONS  ./hello_jobstep.exe
+srun -N $SLURM_JOB_NUM_NODES -n $SLURM_NTASKS -c $OMP_NUM_THREADS ./hello_jobstep.exe | sort
