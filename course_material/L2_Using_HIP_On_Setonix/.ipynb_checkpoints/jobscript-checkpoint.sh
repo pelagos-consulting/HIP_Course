e3980ac30e6d302cb84c17b674b02f79806cb111
Toby Potter <tobympotter@gmail.com>
2023-04-20 15:03:32 +1000

    Latest

--- course_material/L2_Using_HIP_On_Setonix/.ipynb_checkpoints/jobscript-checkpoint.sh
+++ course_material/L2_Using_HIP_On_Setonix/.ipynb_checkpoints/jobscript-checkpoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
 
-#SBATCH --account=${PAWSEY_PROJECT}-gpu       # your account
+#SBATCH --account=<account>-gpu    # your account
 #SBATCH --partition=gpu            # Using the gpu partition
 #SBATCH --ntasks=8                 # Total number of tasks
 #SBATCH --ntasks-per-node=8        # Set this for 1 mpi task per compute device
 #SBATCH --cpus-per-task=8          # How many OpenMP threads per MPI task
-#SBATCH --threads-per-core=1       # How many OpenMP threads per core (1 or 2)
+#SBATCH --threads-per-core=1       # How many OpenMP threads per core
 #SBATCH --gpus-per-task=1          # How many HIP compute devices to allocate to a  task
 #SBATCH --gpu-bind=closest         # Bind each MPI taks to the nearest GPU
 #SBATCH --mem=4000M                #Indicate the amount of memory per node when asking for share resources
@@ -22,6 +22,9 @@ export OMP_PROC_BIND=close  #To bind (fix) threads (allocating them as close as 
 # Temporal workaround for avoiding Slingshot issues on shared nodes:
 export FI_CXI_DEFAULT_VNI=$(od -vAn -N4 -tu < /dev/urandom)
 
+# Compile the software
+make clean
+make
+
 # Run a job with task placement and $BIND_OPTIONS
-#srun -N $SLURM_JOB_NUM_NODES -n $SLURM_NTASKS -c $OMP_NUM_THREADS $BIND_OPTIONS  ./hello_jobstep.exe
 srun -N $SLURM_JOB_NUM_NODES -n $SLURM_NTASKS -c $OMP_NUM_THREADS ./hello_jobstep.exe | sort
