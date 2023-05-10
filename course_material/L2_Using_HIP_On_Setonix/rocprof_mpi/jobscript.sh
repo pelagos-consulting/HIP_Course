528b27f8908f23260b76dc272d2e06153ff02074
Toby Potter <tobympotter@gmail.com>
2023-05-10 16:56:20 +1000

    Latest

--- course_material/L2_Using_HIP_On_Setonix/rocprof_mpi/jobscript.sh
+++ course_material/L2_Using_HIP_On_Setonix/rocprof_mpi/jobscript.sh
@@ -1,15 +1,14 @@
 #!/bin/bash -l
 
-#SBATCH --account=<account>-gpu       # your account
+#SBATCH --account=<account>-gpu    # your account
 #SBATCH --partition=gpu            # Using the gpu partition
-#SBATCH --ntasks=8                 # Total number of tasks
-#SBATCH --ntasks-per-node=8        # Set this for 1 mpi task per compute device
+#SBATCH --ntasks=2                 # Total number of tasks
+#SBATCH --ntasks-per-node=2        # Set this for 1 mpi task per compute device
 #SBATCH --cpus-per-task=8          # How many OpenMP threads per MPI task
 #SBATCH --threads-per-core=1       # How many omp threads per core
-#SBATCH --gpus-per-node=8          # How many GPU's per node
-#SBATCH --gpus-per-task=1          # How many HIP compute devices to allocate to a  task
+#SBATCH --gpus-per-task=1          # How many HIP compute devices to allocate to a task
 #SBATCH --gpu-bind=closest         # Bind each MPI taks to the nearest GPU
-#SBATCH --mem=4000M                #Indicate the amount of memory per node when asking for share resources
+#SBATCH --mem=4000M                # Indicate the amount of memory per node when asking for share resources
 #SBATCH --time=01:00:00
 
 module swap PrgEnv-gnu PrgEnv-cray
@@ -24,7 +23,8 @@ export OMP_PROC_BIND=close  #To bind (fix) threads (allocating them as close as 
 export FI_CXI_DEFAULT_VNI=$(od -vAn -N4 -tu < /dev/urandom)
 
 # compile the code
-srun CC -x hip mat_mult_profiling.cpp -I../../include -o mat_mult_profiling.exe
+make clean
+make
 
 # Make the result directory
 mkdir -p rocprof_counters
--- course_material/L2_Using_HIP_On_Setonix/rocprof_mpi/profile.sh
+++ course_material/L2_Using_HIP_On_Setonix/rocprof_mpi/profile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rocprof -i rocprof_counters.txt -o rocprof_counters/result-$SLURM_JOBID-$SLURM_PROCID.csv ./mat_mult_profiling.exe
+rocprof -i rocprof_counters.txt -o rocprof_counters/result-$SLURM_JOBID-$SLURM_PROCID.csv ./mat_mult_profiling_mpi.exe
