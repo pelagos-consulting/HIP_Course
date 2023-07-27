1ac96b2511a6976f7c0a1ab66df980db9d1965b3
Toby Potter <tobympotter@gmail.com>
2023-07-27 14:51:42 +1000

    Latest

--- course_material/L8_IO_Optimisation/make_traces.sh
+++ course_material/L8_IO_Optimisation/make_traces.sh
@@ -6,7 +6,7 @@ mkdir -p $experiment_dir
 
 # Run rocprof to make traces
 rocprof --hip-trace --hsa-trace -o $experiment_dir/wave2d_sync.csv ./wave2d_sync.exe
-rocprof --hip-trace --hsa-trace -o $experiment_dir/wave2d_async.csv ./wave2d_async.exe
-
+rocprof --hip-trace --hsa-trace -o $experiment_dir/wave2d_async_streams.csv ./wave2d_async_streams.exe
+rocprof --hip-trace --hsa-trace -o $experiment_dir/wave2d_async_events.csv ./wave2d_async_events.exe
 
 
