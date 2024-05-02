fafba27e7084116e544485b5f109ea05cbc91fc1
Toby Potter <tobympotter@gmail.com>
2024-05-02 15:56:48 +1000

    latest

--- course_material/build/L1_Introduction/CMakeFiles/hello_devices.exe.dir/DependInfo.cmake
+++ course_material/build/L1_Introduction/CMakeFiles/hello_devices.exe.dir/DependInfo.cmake
@@ -4,28 +4,11 @@ set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
 
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "HIP"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_HIP
-  "/scratch/courses01/tpotter/HIP_Course/course_material/L1_Introduction/hello_devices.cpp" "/scratch/courses01/tpotter/HIP_Course/course_material/build/L1_Introduction/CMakeFiles/hello_devices.exe.dir/hello_devices.cpp.o"
-  )
-set(CMAKE_HIP_COMPILER_ID "Clang")
-
-# Preprocessor definitions for this target.
-set(CMAKE_TARGET_DEFINITIONS_HIP
-  "USE_PROF_API=1"
-  "__HIP_PLATFORM_AMD__=1"
-  "__HIP_PLATFORM_HCC__=1"
-  )
-
-# The include file search paths:
-set(CMAKE_HIP_TARGET_INCLUDE_PATH
-  "/software/setonix/2023.08/pawsey/software/rocm/gcc/12.2.0/rocm-5.4.3rev1/llvm/lib/clang/15.0.0/include/.."
   )
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/toby/Pelagos/Projects/HIP_Course/course_material/L1_Introduction/hello_devices.cpp" "L1_Introduction/CMakeFiles/hello_devices.exe.dir/hello_devices.cpp.o" "gcc" "L1_Introduction/CMakeFiles/hello_devices.exe.dir/hello_devices.cpp.o.d"
   )
 
 # Targets to which this target links.
