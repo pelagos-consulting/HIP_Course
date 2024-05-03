5d1787e618ca83849c0b3c509b541980d4877df3
Toby Potter <tobympotter@gmail.com>
2024-05-03 14:35:12 +1000

    latest

--- course_material/install.sh
+++ course_material/install.sh
@@ -3,19 +3,34 @@
 # Get the location of this script
 script_path=$(dirname "$0")
 
-source $script_path/env
+env_script=$(realpath $script_path/env) 
+
+source $env_script
 
 # Make the directory to build in
-mkdir -p $script_path/build
+mkdir -p $COURSE_DIR/build
 
-#rm -rf $script_path/build/*
-
-cd $script_path/build
-
+cd $COURSE_DIR/build
 
 # Run cmake
 cmake -DCMAKE_INSTALL_MESSAGE=LAZY -DCMAKE_VERBOSE_MAKEFILE=OFF -DCMAKE_RULE_MESSAGES=OFF -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR/ -DCMAKE_BUILD_TYPE=$BUILD_TYPE $COURSE_DIR/
 
 #make clean
-make install
+make all install
 
+build_script=${RUN_DIR}/build
+
+# Make a build script
+cat << EOF > ${build_script}
+#!/bin/bash
+
+source ${env_script}
+
+
+
+
+
+EOF
+
+# Change permissions
+chmod u+x ${build_script} 
