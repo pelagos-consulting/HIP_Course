e92eab375ce69e7c6d30bf251d588d9e1cf5be76
Toby Potter <tobympotter@gmail.com>
2024-05-10 12:27:51 +1000

    Latest

--- course_material/L2_Using_HIP_On_Setonix/rocprof_mpi/jobscript.sh
+++ course_material/L2_Using_HIP_On_Setonix/rocprof_mpi/jobscript.sh
@@ -3,12 +3,12 @@
 #SBATCH --account=<account>-gpu    # your account
 #SBATCH --partition=gpu-dev        # Using the gpu-dev partition
 #SBATCH --nodes=1                  # Total number of nodes
-#SBATCH --gpus-per-node=2          # Number of GPU's per node
+#SBATCH --gres:gpu=2               # Number of GPU's per node
 #SBATCH --time=01:00:00
 
 module swap PrgEnv-gnu PrgEnv-cray
 module load craype-accel-amd-gfx90a
-module load rocm
+module load rocm/5.2.3
 
 export OMP_NUM_THREADS=8   #To define the number of OpenMP threads available per MPI task
 export OMP_PLACES=cores     #To bind to cores 
@@ -25,5 +25,5 @@ make
 mkdir -p rocprof_counters
 
 # Run the profiling job
-srun -N $SLURM_JOB_NUM_NODES -n 2 --gpus-per-task=1 --gpus-per-node=2 --gpu-bind=closest -c $OMP_NUM_THREADS ./profile.sh
+srun -N $SLURM_JOB_NUM_NODES -n 2 -c 8 --gpus-per-task=1 --gpu-bind=closest -c $OMP_NUM_THREADS ./profile.sh
 
