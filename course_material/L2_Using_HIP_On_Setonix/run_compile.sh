39e5fe936a76908bbce2142bc92f4fb53c287c0a
Toby Potter <tobympotter@gmail.com>
2024-05-10 12:33:59 +1000

    latest

--- course_material/L2_Using_HIP_On_Setonix/run_compile.sh
+++ course_material/L2_Using_HIP_On_Setonix/run_compile.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -login
-
 # Use a login shell to have a clean environment
-module load rocm/5.2.3
+
+# Get the environment
+source ../env
+
+# Swap modules
 module swap PrgEnv-gnu PrgEnv-cray
-module load craype-accel-amd-gfx90a
 
 # Run the make command
 make clean
