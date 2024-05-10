500dd9ff3399e2a8d64876a0469c36ee21ed32af
Toby Potter <tobympotter@gmail.com>
2024-05-10 12:18:44 +1000

    Latest

--- course_material/L2_Using_HIP_On_Setonix/run_compile.sh
+++ course_material/L2_Using_HIP_On_Setonix/run_compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -login
 
 # Use a login shell to have a clean environment
-module load rocm/5.4.3
+module load rocm/5.2.3
 module swap PrgEnv-gnu PrgEnv-cray
 module load craype-accel-amd-gfx90a
 
