8a513e1c4e97bf9641a6588261d273d00ad83ceb
drtpotter <tobympotter@gmail.com>
2024-05-16 07:38:33 +0800

    latest

--- course_material/L2_Using_HIP_On_Setonix/rocprof_mpi/jobscript.sh
+++ course_material/L2_Using_HIP_On_Setonix/rocprof_mpi/jobscript.sh
@@ -1,10 +1,12 @@
-##!/bin/bash -l
+##!/bin/bash --login
 
-#SBATCH --account=<account>-gpu    # your account
+#SBATCH --account=courses01-gpu    # your account
 #SBATCH --partition=gpu-dev        # Using the gpu-dev partition
 #SBATCH --nodes=1                  # Total number of nodes
 #SBATCH --gres:gpu=2               # Number of GPU's per node
 #SBATCH --time=01:00:00
+
+echo "Hostname is $HOSTNAME"
 
 source ../../env
 
@@ -15,13 +17,8 @@ export OMP_PROC_BIND=close  #To bind (fix) threads (allocating them as close as 
 # Temporal workaround for avoiding Slingshot issues on shared nodes:
 export FI_CXI_DEFAULT_VNI=$(od -vAn -N4 -tu < /dev/urandom)
 
-# compile the code
-make clean
-make
-
 # Make the result directory
 mkdir -p rocprof_counters
 
 # Run the profiling job
 srun -N $SLURM_JOB_NUM_NODES -n 2 -c 8 --gpus-per-task=1 --gpu-bind=closest ./profile.sh
-
