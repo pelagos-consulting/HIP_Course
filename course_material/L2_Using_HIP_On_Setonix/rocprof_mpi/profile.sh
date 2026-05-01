da5ef73a42b81ff72628e7ba5cc6c9b0cc51f7db
Toby Potter <tobympotter@gmail.com>
2026-05-01 11:33:29 +1000

    latest

--- course_material/L2_Using_HIP_On_Setonix/rocprof_mpi/profile.sh
+++ course_material/L2_Using_HIP_On_Setonix/rocprof_mpi/profile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rocprof -i rocprof_counters.txt -o rocprof_counters/result-$SLURM_JOBID-$SLURM_PROCID.csv mat_mult_profiling_mpi.exe
+rocprofv3 -i rocprof_counters.txt -o rocprof_counters/result-$SLURM_JOBID-$SLURM_PROCID -- mat_mult_profiling_mpi.exe
